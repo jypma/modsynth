b7849123ffd40a95ae41ac5c9737a42cd54ac7c0
Jan Ypma <jan@ypmania.nl>
2022-08-29 14:36:55 +0200

    Few faceplate cleanups to get rid of warnings

--- export.sh
+++ export.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
 
-export USER_ID=$(id -u)
-export GROUP_ID=$(id -g)
-export WORKDIR=.
-export SUBDIR=QuadAttenuverter/ioboard
+set -e
 VERSION=ki6.0.7_Debian
 
+function handleDir {
+    USER_ID=$(id -u)
+    GROUP_ID=$(id -g)
+    WORKDIR=.
+    SUBDIR=$1
 
-docker run --rm -it -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY \
-    -v $(pwd)/$WORKDIR:/home/$USER/workdir \
-    --user $USER_ID:$GROUP_ID \
-    --env NO_AT_BRIDGE=1 \
-    --workdir="/home/$USER" \
-    --volume="/home/$USER/.local/share/kicad:/home/$USER/.local/share/kicad" \
-    --volume="/home/$USER/.cache/kicad_v6.0:/home/$USER/.cache/kicad_v6.0" \
-    --volume="/home/$USER/.cache/kicad:/home/$USER/.cache/kicad" \
-    --volume="/home/$USER/.cache/kicost:/home/$USER/.cache/kicost" \
-    --volume="/home/$USER/.config/kicad:/home/$USER/.config/kicad:rw" \
-    --volume="/etc/group:/etc/group:ro" \
-    --volume="/etc/passwd:/etc/passwd:ro" \
-    --volume="/etc/shadow:/etc/shadow:ro" \
-    setsoft/kicad_auto_test:${VERSION} /bin/bash -c "cd workdir/$SUBDIR; kibot"
+    cp kibot_generated.kibot.yaml $SUBDIR
+    docker run --rm -it -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY \
+           -v $(pwd)/$WORKDIR:/home/$USER/workdir \
+           --user $USER_ID:$GROUP_ID \
+           --env NO_AT_BRIDGE=1 \
+           --workdir="/home/$USER" \
+           --volume="/home/$USER/.local/share/kicad:/home/$USER/.local/share/kicad" \
+           --volume="/home/$USER/.cache/kicad_v6.0:/home/$USER/.cache/kicad_v6.0" \
+           --volume="/home/$USER/.cache/kicad:/home/$USER/.cache/kicad" \
+           --volume="/home/$USER/.cache/kicost:/home/$USER/.cache/kicost" \
+           --volume="/home/$USER/.config/kicad:/home/$USER/.config/kicad:rw" \
+           --volume="/etc/group:/etc/group:ro" \
+           --volume="/etc/passwd:/etc/passwd:ro" \
+           --volume="/etc/shadow:/etc/shadow:ro" \
+           setsoft/kicad_auto_test:${VERSION} /bin/bash -c "cd workdir/$SUBDIR; kibot -v"
+    rm ${SUBDIR}/kibot_generated.kibot.yaml
+}
+
+#handleDir QuadAttenuverter/ioboard
+#handleDir QuadAttenuverter/mainboard
+handleDir QuadAttenuverter/faceplate
